b252de25f28180cae86ba603446b296117b0ea84
Danylo79 <dan.komisarenko@gmail.com>
2021-02-12 11:50:59 -0500

    Refactoring (Because old code bad D:)

--- plight-core/src/test/java/dev/dankom/util/general/StringFormatterTest.java
+++
@@ -1,9 +0,0 @@
-package dev.dankom.util.general;
-
-import junit.framework.TestCase;
-
-public class StringFormatterTest extends TestCase {
-    public void testJoin() {
-        assertEquals("foobar", StringFormatter.join("foo", "bar"));
-    }
-}
